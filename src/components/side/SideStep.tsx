4b302623c3cc26bc9810a56a2e4f33831f1da7a8
alvaromd99 <117214600+alvaromd99@users.noreply.github.com>
2023-12-15 13:28:28 +0100

    add css to the side bar steps

--- src/components/side/SideStep.tsx
+++ src/components/side/SideStep.tsx
@@ -6,7 +6,9 @@ interface SideStepProps {
 const SideStep = ({ order, text }: SideStepProps) => {
 	return (
 		<div className='step'>
-			<div className='step-number'>{order}</div>
+			<div className='step-number'>
+				<p>{order}</p>
+			</div>
 			<div className='step-content'>
 				<p>{`Step ${order}`}</p>
 				<h2>{text}</h2>
