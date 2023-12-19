1f6c9d83349936696b2d32bafe80d412bd2c6cfb
alvaromd99 <117214600+alvaromd99@users.noreply.github.com>
2023-12-19 11:05:25 +0100

    style the form

--- src/components/step1/CustomInput.tsx
+++ src/components/step1/CustomInput.tsx
@@ -1,3 +1,66 @@
-export default function CustomInput() {
-	return <div>CustomInput</div>
+import { ChangeEvent, useState } from 'react'
+
+interface CustomInputProps {
+	id: string
+	label: string
+	type: string
+	placeholder: string
+}
+
+export default function CustomInput({
+	id,
+	label,
+	type,
+	placeholder,
+}: CustomInputProps) {
+	const [value, setValue] = useState('')
+	const [error, setError] = useState('')
+
+	const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+		const inputValue = event.target.value
+		setValue(inputValue)
+
+		if (id === 'name') {
+			if (inputValue.length < 2 && inputValue !== '') {
+				setError('Name should be at least 2 characters')
+			} else {
+				setError('')
+			}
+		} else if (id === 'email') {
+			const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+
+			if (!emailPattern.test(inputValue)) {
+				setError('Invalid email')
+			} else {
+				setError('')
+			}
+		} else if (id === 'phoneNumber') {
+			const phonePattern = /^\+\d{2}\s\d{3}\s\d{3}\s\d{3}$/
+
+			if (!phonePattern.test(inputValue)) {
+				setError('Use format: +12 234 567 890')
+			} else {
+				setError('')
+			}
+		} else {
+			setError('')
+		}
+	}
+
+	return (
+		<div className='custom-input'>
+			<div className='custom-input-info'>
+				<label htmlFor={id}>{label}</label>
+				{error && <span style={{ color: 'red' }}>{error}</span>}
+			</div>
+			<input
+				value={value}
+				id={id}
+				type={type}
+				placeholder={placeholder}
+				onChange={handleChange}
+				autoComplete='off'
+			/>
+		</div>
+	)
 }
--- src/components/step1/InfoForm.tsx
+++ src/components/step1/InfoForm.tsx
@@ -1,12 +1,33 @@
 import Title from '../header/Title'
+import CustomInput from './CustomInput'
 
 const InfoForm = () => {
 	return (
-		<div>
+		<div className='step1'>
 			<Title
 				title='Personal Info'
 				subtitle='Please provide your name, email address and phone number.'
 			/>
+			<div className='form-content'>
+				<CustomInput
+					id='name'
+					label='Name'
+					type='text'
+					placeholder='e.g. Stephen King'
+				/>
+				<CustomInput
+					id='email'
+					label='Email Address'
+					type='email'
+					placeholder='e.g. stephen@example.com'
+				/>
+				<CustomInput
+					id='phoneNumber'
+					label='Phone Number'
+					type='text'
+					placeholder='e.g. +1 234 567 890'
+				/>
+			</div>
 		</div>
 	)
 }
