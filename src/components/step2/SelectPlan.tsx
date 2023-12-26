57b8c2c05d46064f1a0598f821a134dcc2310ed0
alvaromd99 <117214600+alvaromd99@users.noreply.github.com>
2023-12-26 13:53:50 +0100

    add year/month pay switch css

--- src/components/step2/SelectPlan.tsx
+++ src/components/step2/SelectPlan.tsx
@@ -1,12 +1,21 @@
 import Footer from '../footer/Footer'
 import Title from '../header/Title'
 import PlanCard from './PlanCard'
+import { useState } from 'react'
 
 import ArcadeIcon from '../../assets/images/icon-arcade.svg'
 import AdvanceIcon from '../../assets/images/icon-advanced.svg'
 import ProIcon from '../../assets/images/icon-pro.svg'
 
 const SelectPlan = () => {
+	const [isChecked, setIsChecked] = useState(false)
+
+	const handleToggleChange = () => {
+		setIsChecked(!isChecked)
+	}
+
+	console.log(isChecked)
+
 	return (
 		<div className='main-content-step step-two'>
 			<Title
@@ -18,10 +27,19 @@ const SelectPlan = () => {
 				<PlanCard title='Advance' price='12' svg={AdvanceIcon} />
 				<PlanCard title='Pro' price='15' svg={ProIcon} />
 			</div>
-			<label className='pay-plan-switch'>
-				<input type='checkbox' />
-				<span className='pay-plan-slider'></span>
-			</label>
+			<div className='pay-plan'>
+				<label className='pay-plan-switch'>
+					<input
+						type='checkbox'
+						id='pay-plan-checkbox'
+						checked={isChecked}
+						onChange={handleToggleChange}
+					/>
+					<span className='left-text'>Monthly</span>
+					<span className='pay-plan-slider'></span>
+					<span className='right-text'>Yearly</span>
+				</label>
+			</div>
 			<Footer order={2} />
 		</div>
 	)
