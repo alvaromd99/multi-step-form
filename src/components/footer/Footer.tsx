a656a59b268468b755d9b50c8530ef3c2360c055
alvaromd99 <117214600+alvaromd99@users.noreply.github.com>
2024-01-10 14:42:17 +0100

    delete the validation on the next btn

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,48 +1,14 @@
-import { useState } from 'react'
-import { FormValuesAndErrorsState } from '../../context/InfoProvider'
-import { useInfo } from '../../hooks/useInfo'
 import { useRender } from '../../hooks/useRender'
-import { usePlans } from '../../hooks/usePlans'
 
 interface FooterProps {
 	order: number
 }
 
 export default function Footer({ order }: FooterProps) {
-	const [error, setError] = useState('')
-
 	const { changeComponentToShow } = useRender()
-	const { formValues } = useInfo()
-	const { planSelected } = usePlans()
 
 	const isDisabled = order === 1
 	const isLast = order === 4
-
-	function allPropertiesEmpty(obj: FormValuesAndErrorsState) {
-		return Object.values(obj).every((prop) => prop === '')
-	}
-
-	const handleComponentChange = () => {
-		if (order === 1) {
-			if (!allPropertiesEmpty(formValues)) {
-				changeComponentToShow(order + 1)
-			} else {
-				setError('Please fill in all required fields.')
-			}
-		}
-
-		if (order === 2) {
-			if (planSelected.name !== '') {
-				changeComponentToShow(order + 1)
-			} else {
-				setError('Please chose a plan.')
-			}
-		}
-
-		if (order === 3) {
-			console.log('3')
-		}
-	}
 
 	return (
 		<div
@@ -55,14 +21,11 @@ export default function Footer({ order }: FooterProps) {
 				style={{ display: isDisabled ? 'none' : 'block' }}>
 				Go Back
 			</button>
-			<div>
-				{error !== '' && <p className='footer-error-msg'>{error}</p>}
-				<button
-					className={`next-btn ${isLast ? 'last' : ''}`}
-					onClick={() => handleComponentChange()}>
-					{isLast ? 'Confirm' : 'Next'}
-				</button>
-			</div>
+			<button
+				className={`next-btn ${isLast ? 'last' : ''}`}
+				onClick={() => changeComponentToShow(order + 1)}>
+				{isLast ? 'Confirm' : 'Next'}
+			</button>
 		</div>
 	)
 }
